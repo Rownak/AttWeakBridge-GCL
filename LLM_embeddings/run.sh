39e369c86399f9a1c424aca1215ea1e9a786f76c
Rownak <rownak.sust@gmail.com>
2025-12-05 07:20:20 -0500

    Update run.sh
    Updated the commands to run the fine tuning of LLMs and extracting the embeddings

--- LLM_embeddings/run.sh
+++ LLM_embeddings/run.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 # Define the model_ids
-model_ids=(3)
+# models = ["jackaduma/SecRoBERTa", "ehsanaghaei/SecureBERT", "gpt2-xl", "gpt2"]
+# model_names = ["SecRoBERTa", "SecureBERT", "gpt2-xl", "gpt2"]
 
-# # Loop over each model_id
-# for model_id in "${model_ids[@]}"; do
-#     echo "Running model_id=$model_id"
-#     python 1_llm_fine_tune.py --model_id $model_id
-#     sleep 2
-# done
+model_ids=(0 3)
+
+# Loop over each model_id
+for model_id in "${model_ids[@]}"; do
+    echo "Running model_id=$model_id"
+    python 1_llm_fine_tune.py --model_id $model_id
+    sleep 2
+done
 
 # Define the model_ids
-model_ids2=(6)
+# models = ["pt_SecRoBERTa", "SecRoBERTa", "pt_SecureBERT", "SecureBERT", "pt_gpt2-xl", "gpt2-xl", "pt_bert"]
+
+model_ids2=(1 5)
 
 # Loop over each model_id
 for model_id in "${model_ids2[@]}"; do
     echo "Running model_id=$model_id"
-    python create_embeddings.py --model_id $model_id
+    python 2_extract_embeddings.py --model_id $model_id
     sleep 2
 done
 
